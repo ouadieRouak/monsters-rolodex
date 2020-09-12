b4a7bab6e1bf1638244eaefe2f69e32f6f8da61c
MacAir <macair@MacBook-Air-de-MacAir.local>
2020-09-12 22:52:16 +0100

    Updates

---
+++ precache-manifest.d0ec8f75651ad1aaf8a6f2f433e173f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43acc8a05e7fcb65d759248c30fb4d46",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a130e2f411480aae216",
+    "url": "/monsters-rolodex/static/css/main.23939379.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a130e2f411480aae216",
+    "url": "/monsters-rolodex/static/js/main.c67fc24c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
